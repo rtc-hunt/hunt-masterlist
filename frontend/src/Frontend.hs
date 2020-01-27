3a71c018cb7a483de655f9af699b769270525b8b
Jonathan D.K. Gibbons <jonored@gmail.com>
2020-01-27 02:30:05 -0500

    Lots of work on the main puzzle table.

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -36,6 +36,10 @@ frontend = Frontend
   { _frontend_head = do
      el "title" $ text "Obelisk Minimal Example"
      elAttr "link" ( "rel" =: "stylesheet" <> "media" =: "screen" <> "href" =: "https://fontlibrary.org/face/symbola" <> "type" =: "text/css") $ blank
+     -- elAttr "link" ( "rel" =: "stylesheet" <> "media" =: "screen" <> "href" =: "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" <> "type" =: "text/css") $ blank
+     elAttr "link" ( "rel" =: "stylesheet" <> "media" =: "screen" <> "href" =: "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/table.min.css" <> "type" =: "text/css") $ blank
+     elAttr "link" ( "rel" =: "stylesheet" <> "media" =: "screen" <> "href" =: "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/popup.min.css" <> "type" =: "text/css") $ blank
+     elAttr "link" ( "rel" =: "stylesheet" <> "media" =: "screen" <> "href" =: "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/icon.min.css" <> "type" =: "text/css") $ blank
   , _frontend_body = do
       el "style" $ text cssText
       route <- fromMaybe "" <$> getTextConfig "common/route"
@@ -56,33 +60,49 @@ tshow = T.pack . show
 puzzleMasterList :: MonadRhyoliteWidget (HMLViewSelector SelectedCount) HMLRequest t m => m ()
 puzzleMasterList = do
   puzzles <- watchPuzzles
-  el "table" $ do
+  elClass "table" "ui celled table" $ do
     el "thead" $ el "tr" $ do
       el "th" $ text "Title"
-      el "th" $ text "Puzzle Page"
-      el "th" $ text "Worksheet"
+      --el "th" $ text "Puzzle Page"
+      --el "th" $ text "Worksheet"
       el "th" $ text "Is Meta?"
-      el "th" $ text "Started At"
+      el "th" $ text "Meta"
       el "th" $ text "Solution(s)"
+      el "th" $ text "Status"
+      el "th" $ text "Current solvers"
+      el "th" $ text "Tags"
+      elAttr "th" ("style" =: "width: 40%") $ text "Notes"
 
-    
+
     dyn_ $ ffor puzzles $ \pzlMap -> forM_ (MMap.toList pzlMap) $ \(pzlId, pzl) -> el "tr" $ do
-      el "td" $ el "h2" $ text $ _puzzle_Title pzl
-      el "td" $ elAttr "a" ("href" =: _puzzle_URI pzl) $ text "Puzzle Page"
-      el "td" $ elAttr "a" ("href" =: "#puzzle" <> "style" =: "font-family 'SymbolaRegular'") $ text "Sheet"
+      (elem) <- el' "td" $ elAttr "div" ("class" =: "tooltip pointer" <> "data-tooltip" =: "Open Puzzle") $ do
+        text $ _puzzle_Title pzl
+        -- elClass "span" "tooltiptext" $ text "Open Puzzle"
+      --el "td" $ elAttr "a" ("href" =: _puzzle_URI pzl) $ text "Puzzle Page"
+      --el "td" $ elAttr "a" ("href" =: "#puzzle" <> "style" =: "font-family 'SymbolaRegular'") $ text "Sheet"
       el "td" $ if _puzzle_IsMeta pzl then text "META" else text ""
-      el "td" $ text $ maybe "Not Started" tshow $ _puzzle_StartedAt pzl
+      el "td" $ text "Unknown" -- if _puzzle_IsMeta pzl then text "META" else text ""
       el "td" $ do
         solutions <- watchSolves $ constDyn $ SolveQuery_byPuzzle $ pzlId
         dyn_ $ ffor solutions $ \solMap -> forM_ (MMap.toList solMap) $ \(solId, sol) -> do
           el "pre" $ do 
             text $ _solve_Solution sol
             if _solve_IsBacksolve sol then backsolve1 else blank
+      el "td" $ text $ maybe "Not Started" tshow $ _puzzle_StartedAt pzl
+      el "td" $ do
+        text "Nobody"
+      el "td" $ do
+        _ <- elClass' "i" "plus icon" $ blank
+        blank
+      el "td" $ do 
+        el "div" $ text "a normal note"
+        elClass "div" "help-wanted" $ text "cryptics"
   return ()
 
 
 cssText :: T.Text
 cssText = LT.toStrict $ Clay.render $ do
+  ".pointer" ? cursor pointer
   ".tooltip" ? do
     Clay.position relative
   ".tooltip" # ":hover" |> ".tooltiptext" ? do
@@ -99,11 +119,19 @@ cssText = LT.toStrict $ Clay.render $ do
   pre ? do
     Clay.padding (em 0) (em 0) (em 0) (em 0)
     Clay.margin (em 0) (em 0) (em 0) (em 0)
+  Clay.div # ".help-wanted" ? do
+    Clay.borderRadius (em 0.5) (em 0.5) (em 0.5) (em 0.5)
+    Clay.background (rgb 255 220 220)
+    Clay.padding (em 0.5) (em 0.5) (em 0.5) (em 0.5)
+  Clay.div # ".help-wanted" # "::before" ? Clay.content (stringContent "HELP: ")
+  Clay.i ? do
+    Clay.fontColor lightgray
+
 
 backsolve1 :: DomBuilder t m => m ()
-backsolve1 = elAttr "span" ("class" =: "tooltip" <> "style" =: "font-family: 'SymbolaRegular'") $ do
+backsolve1 = elAttr "span" ("class" =: "tooltip" <> "style" =: "font-family: 'SymbolaRegular'" <> "data-tooltip" =: "This solution was backsolved.") $ do
   text " 🝡⃪"
-  elClass "span" "tooltiptext" $ text "This solution was backsolved."
+--  elClass "span" "tooltiptext" $ text "This solution was backsolved."
 
 backsolve2 :: DomBuilder t m => m ()
 backsolve2 = elAttr "span" ("style" =: "font-family: 'SymbolaRegular'") $ do
