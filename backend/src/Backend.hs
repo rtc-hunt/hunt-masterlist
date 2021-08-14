ae19e3317565ac33a199e518fab30f58fd0dee9e
Ali Abrar <ali@abrar.nyc>
2021-08-13 21:27:49 -0400

    Spin up the database

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -2,10 +2,22 @@ module Backend where
 
 import Backend.Schema
 import Common.Route
+import Control.Monad.Logger
+import Data.Functor.Identity
+import Database.Groundhog (runMigration)
+import Database.Groundhog.Generic.Migration (getTableAnalysis)
+import Gargoyle.PostgreSQL.Connect
 import Obelisk.Backend
+import Rhyolite.Backend.DB
 
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
-  { _backend_run = \serve -> serve $ const $ return ()
+  { _backend_run = \serve -> do
+    withDb "db" $ \db -> runNoLoggingT $ do
+      runDb (Identity db) $ do
+        tables <- getTableAnalysis
+        runMigration $ migrateSchema tables
+      return ()
+    serve $ const $ return ()
   , _backend_routeEncoder = fullRouteEncoder
   }
